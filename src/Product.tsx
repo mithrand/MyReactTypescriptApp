c723d033be2b02678c7f512131fec069b7005ce7
Antonio <munoztorres.antonio@gmail.com>
2017-02-21 23:30:41 +0100

    Con acceso a datos

--- src/Product.tsx
+++ src/Product.tsx
@@ -2,23 +2,44 @@
  * Created by Antonio on 21/02/2017.
  */
 import * as React from 'react';
+import {IProduct} from './Seed';
 
-class Product extends  React.Component<{}, {}> {
+class Product extends  React.Component<IProduct, {}> {
+
+    private producto: IProduct;
+
+    constructor(props: IProduct) {
+        super(props);
+        this.producto = props;
+    }
 
     public render () {
         return (
             <div className="item">
                 <div className="image">
-                    <img src="images/products/image-aqua.png" />
+                    <img src={this.props.productImageUrl} />
                 </div>
                 <div className="middle aligned content">
+                    <div className="header">
+                        <a>
+                            <i className="large caret up icon" />
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className="description">
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
                     </div>
                     <div className="extra">
                         <span>Submitted by:</span>
-                        <img className="ui avatar image" src="images/avatars/daniel.jpg"/>
+                        <img
+                            className="ui avatar image"
+                            src={this.props.submitterAvatarUrl}
+                        />
                     </div>
                 </div>
             </div>
--- src/ProductList.tsx
+++ src/ProductList.tsx
@@ -3,13 +3,26 @@
  */
 import * as React from 'react';
 import Product from './Product';
+import {Datos, IProduct} from './Seed';
+
+const productos = Datos.getProducts();
 
 class ProductList extends  React.Component<{}, {}> {
+
+    private product: IProduct = productos[0];
 
     public render() {
         return (
             <div className="ui unstackable items">
-               <Product />
+               <Product
+                   id={this.product.id}
+                   title={this.product.title}
+                   description={this.product.description}
+                   url={this.product.url}
+                   votes={this.product.votes}
+                   submitterAvatarUrl={this.product.submitterAvatarUrl}
+                   productImageUrl={this.product.productImageUrl}
+               />
             </div>
         );
     }
